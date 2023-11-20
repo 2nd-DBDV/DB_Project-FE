b8e3d1f47eed68a38ebb316dcea3cfe7a8f1c6a0
syw2045 <syw2045@naver.com>
2023-11-20 19:39:57 +0900

    initial setting

---
+++ db-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// 스타일 정의
+const TopStyle = styled.div`
+  color: #333;
+  font-size: 18px;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Home = () => {
+  return (
+    <TopStyle>
+      <p>따릉이 메인 페이지!!</p>
+    </TopStyle>
+  );
+}
+
+export default Home;
