74875e40819057719057bc88dd8c047c8d4e8930
syw2045 <syw2045@naver.com>
2023-11-30 18:25:02 +0900

    Footer 수정

--- db-frontend/src/components/Footer.jsx
+++ db-frontend/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const FooterContainer = styled.div`
+  bottom: 0;
   width: 100%;
   padding: 10px 20% 10px 20%;
   text-align: center;
